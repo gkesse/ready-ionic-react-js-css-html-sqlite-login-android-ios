a473c51efe035cb7eecdd1e695efec766a639731
Gerard KESSE <tiakagerard@hotmail.com>
2023-10-25 13:40:38 +0200

    module main

---
+++ app/cmd/envs.sh
@@ -0,0 +1,6 @@
+#================================================
+export "LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH"
+#================================================
+export "GPROJECT_ENV=TEST"
+export "GPROJECT_DATA=/var/lib/readydev"
+#================================================
